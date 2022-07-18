ed2bee1071482b4874ccd553da5bb02be6ed01f6
Abdul Basit <abdulbasit5361234@gmail.com>
2022-07-18 10:29:58 +0500

    Initial commit

---
+++ src/main/kotlin/com/example/demo/DemoAction1.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by abdulbasit on 18/07/2022.
+ */
+public class DemoAction1 extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        //do something
+    }
+}
---
+++ src/main/kotlin/com/example/demo/PopupDialogAction.java
@@ -0,0 +1,44 @@
+// Copyright 2000-2022 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package com.example.demo;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.pom.Navigatable;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+
+public class PopupDialogAction extends AnAction {
+
+  public PopupDialogAction() {
+    super();
+  }
+
+  @Override
+  public void actionPerformed(@NotNull AnActionEvent event) {
+    // Using the event, create and show a dialog
+    Project currentProject = event.getProject();
+    StringBuilder dlgMsg = new StringBuilder(event.getPresentation().getText() + " Selected!");
+    String dlgTitle = event.getPresentation().getDescription();
+    // If an element is selected in the editor, add info about it.
+    Navigatable nav = event.getData(CommonDataKeys.NAVIGATABLE);
+    if (nav != null) {
+      dlgMsg.append(String.format("\nSelected Element: %s", nav.toString()));
+    }
+    Messages.showMessageDialog(currentProject, dlgMsg.toString(), dlgTitle, Messages.getInformationIcon());
+  }
+
+  @Override
+  public void update(AnActionEvent e) {
+    e.getPresentation().setEnabledAndVisible(true);
+  }
+
+
+
+}
